de290dbe8e8f6548833cc1cb2d01e91f67815383
Lexi Rose <north@tinymagnet.com>
2025-04-15 13:34:14 -0500

    Update GameState.cs

--- GameState.cs
+++ GameState.cs
@@ -14,14 +14,34 @@ public enum GameState
 
     Error = -1,
     Unmodded = -2,
-    NotFocused = -3,
-    Loading = -4,
-    Paused = -5,
-    WrongMode = -6,
-    SafeArea = -7,
-    Cutscene = -8,
-    BadPlayerState = -9,
-    Menu = -10,
-    Map = -11,
-    PipelineBusy = -12
+    BadGameSettings = -3,
+    WrongVersion = -4,
+    NotFocused = -5,
+    Loading = -6,
+    Paused = -7,
+    WrongMode = -8,
+    SafeArea = -9,
+    UntimedArea = -10,
+    Cutscene = -11,
+    BadPlayerState = -12,
+    Menu = -13,
+
+    /// <remarks>
+    /// This should only be used when the game is in a map scene and shouldn't be.
+    /// If the map state is correct, return <see cref="Ready"/> instead.
+    /// </remarks>
+    Map = -14,
+
+    /// <remarks>
+    /// This should only be used when the game is in a combat scene and shouldn't be.
+    /// If the combat state is correct, return <see cref="Ready"/> instead.
+    /// </remarks>
+    InCombat = -15,
+
+    /// <remarks>
+    /// This should only be used when the game is not in a combat scene and should be.
+    /// If the non-combat state is correct, return <see cref="Ready"/> instead.
+    /// </remarks>
+    NotInCombat = -16,
+    PipelineBusy = -128
 }
