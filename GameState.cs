3012711bb63a015ba8d52c353faaf85a9dddffde
Lexi Rose <north@tinymagnet.com>
2025-05-09 13:04:29 -0500

    documentation

--- GameState.cs
+++ GameState.cs
@@ -3,45 +3,83 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace ConnectorLib.JSON;
 
+/// <summary>Represents the state of a game.</summary>
 //if you update this, also update the one in CrowdControl.Games
 [SuppressMessage("ReSharper", "UnusedMember.Global")]
 [JsonConverter(typeof(CamelCaseStringEnumConverter))]
 public enum GameState
 {
+    /// <summary>The game state is unknown.</summary>
+    /// <remarks>This is used when the game is not running or the state cannot be determined due to effect pack or connector problems.</remarks>
     Unknown = 0,
 
+    /// <summary>The game is running and is ready to receive effects.</summary>
+    /// <remarks>This is the only game state where effects can be processed.</remarks>
     Ready = 1,
 
+    /// <summary>The game is in an errored or crashed state.</summary>
     Error = -1,
+    
+    /// <summary>The game does not have the required mod(s) installed.</summary>
     Unmodded = -2,
+    
+    /// <summary>The current game settings are not compatible with Crowd Control.</summary>
     BadGameSettings = -3,
+    
+    /// <summary>The game is the wrong version.</summary>
     WrongVersion = -4,
+    
+    /// <summary>The game is not focused.</summary>
     NotFocused = -5,
+    
+    /// <summary>The game is in a loading state.</summary>
     Loading = -6,
+    
+    /// <summary>The game is paused.</summary>
     Paused = -7,
+    
+    /// <summary>The game is in a state that is not compatible with Crowd Control.</summary>
+    /// <remarks>This is used when the game is in a state such as a title menu or alternate game mode.</remarks>
     WrongMode = -8,
+    
+    /// <summary>The game is in a safe area.</summary>
+    /// <remarks>This is used when the game is in a safe area such as a town or hub.</remarks>
     SafeArea = -9,
+    
+    /// <summary>The game is in an untimed area.</summary>
+    /// <remarks>This is used when the game is in an untimed area such as a cutscene or tutorial.</remarks>
     UntimedArea = -10,
+    
+    /// <summary>The game is in a cutscene.</summary>
     Cutscene = -11,
+    
+    /// <summary>The player is not in a state to receive effects such as being dead or stunned.</summary>
     BadPlayerState = -12,
+    
+    /// <summary>The game is in a menu other than the pause menu.</summary>
     Menu = -13,
-
+    
+    /// <summary>The game is in a map state such as a world map or overworld.</summary>
     /// <remarks>
     /// This should only be used when the game is in a map scene and shouldn't be.
     /// If the map state is correct, return <see cref="Ready"/> instead.
     /// </remarks>
     Map = -14,
-
+    
+    /// <summary>The game is in a combat state such as a boss fight or enemy encounter.</summary>
     /// <remarks>
     /// This should only be used when the game is in a combat scene and shouldn't be.
     /// If the combat state is correct, return <see cref="Ready"/> instead.
     /// </remarks>
     InCombat = -15,
-
+    
+    /// <summary>The game is in a non-combat scene such as a cutscene or tutorial.</summary>
     /// <remarks>
     /// This should only be used when the game is not in a combat scene and should be.
     /// If the non-combat state is correct, return <see cref="Ready"/> instead.
     /// </remarks>
     NotInCombat = -16,
+    
+    /// <summary>The game message queue is busy or full and cannot process any more messages.</summary>
     PipelineBusy = -128
 }
--- StandardErrors.cs
+++ StandardErrors.cs
@@ -4,77 +4,125 @@ using Newtonsoft.Json;
 namespace ConnectorLib.JSON;
 
 [JsonConverter(typeof(CamelCaseStringEnumConverter))]
+//remember to copy updates into CrowdControl.Games.StandardErrors
 public enum StandardErrors
 {
+    //General Errors
+    /// <summary>An unknown error occurred.</summary>
     Unknown = 0x0000,
+    /// <summary>An exception was thrown while attempting to run the effect.</summary>
     ExceptionThrown = 0x0001,
 
     //Bad Request
+    /// <summary>The request was invalid.</summary>
     BadRequest = 0x1000,
-
+    /// <summary>The effect was not available.</summary>
     [Obsolete($"Use {nameof(EffectUnknown)} instead.")]
     UnknownEffect = 0x1001,
+    /// <summary>The effect was not available.</summary>
     EffectUnknown = 0x1001,
+    /// <summary>The effect was disabled.</summary>
     EffectDisabled = 0x1002,
+    /// <summary>The effect has already failed.</summary>
     AlreadyFailed = 0x1003,
+    /// <summary>A supplied parameter could not be parsed.</summary>
     CannotParseNumber = 0x1010,
+    /// <summary>A supplied parameter was unknown.</summary>
     UnknownSelection = 0x1011,
 
     //Connector Errors
+    /// <summary>An unknown connector error occurred.</summary>
     ConnectorError = 0x2000,
+    /// <summary>The connector could not read from the game.</summary>
     ConnectorReadFailure = 0x2001,
+    /// <summary>The connector could not write to the game.</summary>
     ConnectorWriteFailure = 0x2002,
+    /// <summary>The connector could not connect to the game.</summary>
     ConnectorNotConnected = 0x2003,
+    /// <summary>The connector does not support the requested effect.</summary>
     ConnectorNotSupported = 0x2004,
 
     //Settings Errors
+    /// <summary>An error occurred while processing the pack settings.</summary>
     SettingsError = 0x3000,
+    /// <summary>The effect is on cooldown.</summary>
     CooldownPerEffect = 0x3101,
+    /// <summary>Effects are on cooldown.</summary>
     CooldownGlobal = 0x3102,
+    /// <summary>The maximum retry time was exceeded.</summary>
     RetryMaxTime = 0x3003,
+    /// <summary>The maximum number of retry attempts was exceeded.</summary>
     RetryMaxAttempts = 0x3004,
 
     //Session Errors
+    /// <summary>No active game session.</summary>
     NoSession = 0x5000,
+    /// <summary>The game session ended before the effect could be run.</summary>
     SessionEnding = 0x5001,
 
     //Game State Errors
+    /// <summary>The game state was not ready.</summary>
     BadGameState = 0x4000,
 
     //Game State Errors - Missing Objects
+    /// <summary>A required game entity was not found.</summary>
     GameObjectNotFound = 0x4100,
+    /// <summary>The player was not found.</summary>
     PlayerNotFound = 0x4101,
+    /// <summary>The character or party member was not found.</summary>
     CharacterNotFound = 0x4102,
+    /// <summary>The enemy was not found.</summary>
     EnemyNotFound = 0x4103,
+    /// <summary>The object was not found.</summary>
     ObjectNotFound = 0x4104,
+    /// <summary>A required object was not found.</summary>
     PrerequisiteNotFound = 0x4105,
 
     //Game State Errors - Object State
+    /// <summary>A required game entity was not in the correct state.</summary>
     ObjectStateError = 0x4200,
+    /// <summary>The character or object was already in the target state.</summary>
     AlreadyInState = 0x4201,
+    /// <summary>The item or object was already acquired.</summary>
     AlreadyAcquired = 0x4202,
+    /// <summary>The selected objective was already finished.</summary>
     AlreadyFinished = 0x4203,
+    /// <summary>There are no empty containers or slots available.</summary>
     NoEmptyContainers = 0x4210,
+    /// <summary>The player's group or party is full.</summary>
     PartyFull = 0x4211,
+    /// <summary>The selected effect is not applicable or not allowed in the current area.</summary>
     InvalidArea = 0x4220,
+    /// <summary>The selected target is not valid for the effect.</summary>
     InvalidTarget = 0x4221,
+    /// <summary>No valid targets were found.</summary>
     NoValidTargets = 0x4222,
+    /// <summary>The selected object or enemy cannot be spawned here.</summary>
     SpawnNotAllowedHere = 0x4224,
 
     //Game State Errors - Range Limits
+    /// <summary>A value range error occurred.</summary>
     RangeError = 0x4300,
-    AlreadyMaximum = 0x4301,
-    AlreadyMinimum = 0x4302,
+    /// <summary>The value was already at the minimum.</summary>
+    AlreadyMinimum = 0x4301,
+    /// <summary>The value was already at the maximum.</summary>
+    AlreadyMaximum = 0x4302,
 
     //Game Pack Errors
+    /// <summary>The effect is not implemented.</summary>
     EffectNotImplemented = 0x6000,
+    /// <summary>A required pack resource was missing.</summary>
     PackResourceMissing = 0x6001,
-
+    
     //Game Pack State Errors
+    /// <summary>A conflicting effect is already running.</summary>
     ConflictingEffectRunning = 0x8000,
+    /// <summary>This effect cannot be queued and another instance is already running.</summary>
     UnqueueablePending = 0x8001,
-
+    
     //Emulator Errors
+    /// <summary>The emulator does not support this effect.</summary>
     EmulatorNotSupported = 0x7000,
+    /// <summary>The emulator was not properly configured.</summary>
     EmulatorInvalidSetting = 0x7001
 }
